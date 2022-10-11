c2bb7efb6f4c0be268f75398928d1e0c77cd9245
sushilmakaju <sushilmakaju559@gmail.com>
2022-10-11 22:17:28 +0600

    adding level and entry feild

--- Gui_test.py
+++ Gui_test.py
@@ -4,4 +4,20 @@ window.title("Gui practice")
 window.geometry("600x600")
 window.resizable(0,False)
 
+##label
+user = Label (window, text = "Username : ",fg="Black")
+user.grid(row = 0, column = 0)
+
+password = Label (window, text = "Password : ",fg="Black")
+password.grid(row = 6, column = 0)
+
+
+
+##text feild
+e1 = Entry(window)
+e1.grid(row=0, column=2)
+
+e2 = Entry(window)
+e2.grid(row=6, column=2)
+
 window.mainloop()
