56ba356e6a09213b29e68bc3ff75e8b8340d4407
sushilmakaju <sushilmakaju559@gmail.com>
2022-10-13 22:33:05 +0600

    added button made it usable

--- Gui_test.py
+++ Gui_test.py
@@ -1,23 +1,31 @@
 from  tkinter import*
+
 window = Tk()
 window.title("Gui practice")
-window.geometry("600x600")
+window.geometry("250x150")
 window.resizable(0,False)
+
 
 ##label
 user = Label (window, text = "Username : ",fg="Black")
-user.grid(row = 0, column = 0)
+user.grid(row = 2, column = 0)
 
 password = Label (window, text = "Password : ",fg="Black")
-password.grid(row = 6, column = 0)
+password.grid(row = 10, column = 0)
 
 
 
 ##text feild
-e1 = Entry(window)
-e1.grid(row=0, column=2)
+user_lbl = Entry(window)
+user_lbl.grid(row=2, column=2)
 
-e2 = Entry(window)
-e2.grid(row=6, column=2)
+pass_lbl = Entry(window)
+pass_lbl.grid(row=10, column=2)
 
+# button
+# login_btn = Button(window,text = "Login", command=window.login)
+# login_btn.grid(row=60, column=2)
+
+close_btn = Button(window,text = "Close",command = window.destroy)
+close_btn.grid(row=90, column=2)
 window.mainloop()
