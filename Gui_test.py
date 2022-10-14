e0e3376b6567a12c48900e3519568fac1038561c
sushilmakaju <sushilmakaju559@gmail.com>
2022-10-14 22:10:27 +0600

    sumbit button functionable

--- Gui_test.py
+++ Gui_test.py
@@ -1,3 +1,4 @@
+import tkinter
 from  tkinter import*
 
 window = Tk()
@@ -7,24 +8,32 @@ window.resizable(0,False)
 
 
 ##label
-user = Label (window, text = "Username : ",fg="Black")
+user = Label (window, text = "name : ",fg="Black")
 user.grid(row = 2, column = 0)
 
-password = Label (window, text = "Password : ",fg="Black")
+password = Label (window, text = "age : ",fg="Black")
 password.grid(row = 10, column = 0)
 
 
 
 ##text feild
-user_lbl = Entry(window)
-user_lbl.grid(row=2, column=2)
+user_var = tkinter.StringVar()# to store value
+user_entry = Entry(window)
+user_entry.grid(row=2, column=2)
 
-pass_lbl = Entry(window)
-pass_lbl.grid(row=10, column=2)
+pass_var = tkinter.StringVar()
+pass_entry = Entry(window)
+pass_entry.grid(row=10, column=2)
 
 # button
-# login_btn = Button(window,text = "Login", command=window.login)
-# login_btn.grid(row=60, column=2)
+def login():
+    user_obj = user_var.get()
+    pass_obj = pass_var.get()
+    print("you are doing great")
+
+sumbit_btn = Button(window,text = "sumbit", command=login)
+sumbit_btn.grid(row=60, column=2)
+
 
 close_btn = Button(window,text = "Close",command = window.destroy)
 close_btn.grid(row=90, column=2)
